46398a9071145107eb4578869f448952c9f6a358
Inho Lee <inhoking@kookmin.ac.kr>
2017-09-27 21:24:25 +0900

    after code review

--- week4/20171675-이인호-assignment4.py
+++ week4/20171675-이인호-assignment4.py
@@ -3,19 +3,24 @@ def fact(n):
         return 1
     else:
         return n * fact(n - 1)
-def fact(m):
-    if m == 0:
+def combination(n, m):
+        return fact(n)/(fact(m)*fact(n - m))
+
+def recursive(n, m):
+    if n == m or m == 0:
         return 1
     else:
-        return m * fact(m - 1)
+        return recursive(n - 1,m - 1) + recursive(n - 1, m)
 while True:
     n = int(input("Enter n:"))
     if n < 0 :
         break
     else:
         m = int(input("Enter m:"))
-        a = int(fact(n)/(fact(m)*fact(n - m)))
-        print("C(%d,%d) = %d" %(n, m, a))
+        print("C(%d,%d) = %d" %(n, m, combination(n, m)))
+        print("C(%d,%d) = %d" %(n, m, recursive(n, m)))
+
+
 
 
 
