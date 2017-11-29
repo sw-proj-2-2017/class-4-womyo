d13caf7ce39163beab6948a4ecca1d37d0319260
Inho Lee <inhoking@kookmin.ac.kr>
2017-11-29 22:34:49 +0900

    after code review

--- week13/guess.py
+++ week13/guess.py
@@ -1,51 +1,30 @@
 class Guess:
-
     def __init__(self, word):
-        self.secretWord = word
         self.guessedChars = []
         self.numTries = 0
-
+        self.secretWord = word
+        self.currentStatus = '_' * (len(self.secretWord))
 
     def display(self):
-        self.count = 0
-        for i, c in enumerate(self.secretWord):
-            if c in self.guessedChars:
-                self.count += 1
-        if self.count == len(self.secretWord):
-            return True
-        else:
-            return False
-
-        self.blanks= ['_']*len(self.secretWord)
-        for i, c in enumerate(self.secretWord):
-            if self.c in self.guessedChars:
-                self.count += 1
-                self.blanks.insert(self.count - 1, c)
-                self.blanks.pop(self.count)
-                if self.count == len(self.secretWord):
-                    return ''.join(str(e) for e in self.blanks)
-            else:
-                self.count += 1
-                self.blanks.insert(self.count - 1, '_')
-                self.blanks.pop(self.count)
-                if self.count == len(self.secretWord):
-                    return ''.join(str(e) for e in blanks)
-
+        print("Current : ", self.currentStatus)
+        print("Tries : ", self.numTries)
 
     def guess(self, character):
-        if character in self.secretWord:
-            if character in self.guessedChars:
-                print(self.display(self))
-            else:
-                self.guessedChars.append(character)
-                print("Tries:", self.numTries)
-                print(self.guessedChars)
-        else:
-            if character in self.guessedChars:
-                print(self.display(self))
-            else:
-                self.guessedChars.append(character)
-                self.numTries += 1
-                print("Tries:", self.numTries)
-                print(self.guessedChars)
+        if character in self.guessedChars:
+            pass
 
+        else:
+            self.guessedChars += character
+
+        if character not in self.secretWord:
+            self.numTries += 1
+
+        for i in range(len(self.secretWord)):
+            if self.secretWord[i] == character:
+                self.currentStatus = self.currentStatus[0: i] + character + self.currentStatus[i + 1:]
+
+        if self.currentStatus == self.secretWord:
+            return True
+
+        else:
+            return False
